59faf77482f01a565c7da10bb3f8e039fcbeaa09
sonic011gamer <efimgirjeu@gmail.com>
2026-06-17 19:29:23 +0300

    msm8909: Use the software gatekeeper/keymaster.

--- msm8909.mk
+++ msm8909.mk
@@ -240,16 +240,12 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl
 
 #Enable QTI KEYMASTER and GATEKEEPER HIDLs
-ifeq ($(ENABLE_VENDOR_IMAGE), true)
-KMGK_USE_QTI_SERVICE := true
-endif
+KMGK_USE_QTI_SERVICE := false
 
 #Enable AOSP KEYMASTER and GATEKEEPER HIDLs
 ifneq ($(KMGK_USE_QTI_SERVICE), true)
-PRODUCT_PACKAGES += android.hardware.gatekeeper@1.0-impl \
-                    android.hardware.gatekeeper@1.0-service \
-                    android.hardware.keymaster@3.0-impl \
-                    android.hardware.keymaster@3.0-service
+PRODUCT_PACKAGES += android.hardware.gatekeeper@1.0-service.software \
+                    android.hardware.keymaster@4.0-service.saana
 endif
 
 SDM660_DISABLE_MODULE := true
